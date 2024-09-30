5fec8c6bbd34b0f3ae11f87c62b28df8e7f1b680
AEmili <andrea.emili@outlook.com>
2024-09-30 09:30:28 +0200

    Update Main.cpp
    project update

--- prj - Hello World/Hello World - C++/Main.cpp
+++ prj - Hello World/Hello World - C++/Main.cpp
@@ -1,16 +1,17 @@
 /*
 * This is a education code project. It is the first project that usually is showed in any course of programming.
-* Last Update: 2024/01/31
+* Last Update: 2024/09/30
 * Author: Andrea Emili (andrea.emili@outlook.com)
 */
 
+// --> This is a comment
 
 #include <iostream>	// This is I/O Library
 
 /*
 * Function: Main
 */
-void main()
+int main()
 {
 	std::cout << "Hello, World" << std::endl;
 }
