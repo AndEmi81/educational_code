9825bc6ecdd0b4612e223669ccb4f77f82af0f9b
AE <andrea.emili@outlook.com>
2025-02-18 12:24:39 +0100

    Added Algoritmi Edu Start

---
+++ Start - Algoritmi/Primo Proramma.cpp
@@ -0,0 +1,13 @@
+/*
+* Progetto: Primo programma
+* Author: Andrea Emili
+* Last review: 2025/02/18
+*/
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Benvenuto" << endl;
+}
